4140a17f1a09301f48df6fdf40bcd1c787ae2cd0
pjchender <pjchender@gmail.com>
2019-07-02 12:03:05 +0800

    Add basic config for react-intl

--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { IntlProvider, addLocaleData } from 'react-intl';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import enLocaleData from 'react-intl/locale-data/en.js';
+import zhLocaleData from 'react-intl/locale-data/zh.js';
+import jaLocaleData from 'react-intl/locale-data/ja.js';
+
+addLocaleData([
+  ...zhLocaleData,
+  ...enLocaleData,
+  ...jaLocaleData
+])
+
+const Root = () => {
+  const locale = navigator.language;
+
+  return (
+    <IntlProvider locale={locale} key={locale} defaultLocale="en">
+      <App />
+    </IntlProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
